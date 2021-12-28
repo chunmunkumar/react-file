5682b88ac482398d6d6688401b0ca2eb52588fc2
chunmunkumar <92389819+chunmunkumar@users.noreply.github.com>
2021-12-28 16:58:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './Heading';
+import SubHeading from './SubHeading';
+import InputQuery from './InputQuery';
+import SubmitButton from './SubmitButton';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Heading/>
+          <SubHeading/>
+          <InputQuery/>
+          <SubmitButton/>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
